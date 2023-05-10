3eb326c4863915d01659f129b50493789558d263
Turmoil <tony-turmoil@qq.com>
2023-05-11 00:12:54 +0800

    update: presentation

--- src/dump.cpp
+++ src/dump.cpp
@@ -5,6 +5,11 @@
 */
 
 #include "dump.h"
+#include <cmath>
+
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 bool GetResult(std::vector<disc_poly_ptr>* polys, std::vector<flp_t>* divs)
 {
@@ -41,12 +46,40 @@ void DumpResult(FILE* fp, std::vector<disc_poly_ptr>* polys, std::vector<flp_t>*
 		return;
 	}
 
-	fprintf(fp, "------------------------------\n");
+#ifdef _WIN32	// print timestamp
+	SYSTEMTIME sysTime;
+	GetLocalTime(&sysTime);
+	fprintf(fp, "==================== ");	// * 20
+	fprintf(fp, "%02hu/%02hu/%02hu ", sysTime.wMonth, sysTime.wDay, sysTime.wYear);
+	fprintf(fp, "%02hu:%02hu:%02hu ", sysTime.wHour, sysTime.wMinute, sysTime.wSecond);
+	fprintf(fp, "====================\n");	// *30
+#else
+	fprintf_s(fp, "Dump ==============================\n");	// *30
+#endif
+
+	fprintf(fp, "        FXP: n = %u, f = %u\n", fxp_n, fxp_f);
+	fprintf(fp, " Max sample: %d\n", MS);
+	fprintf(fp, "Max segment: %d\n", m_MAX);
+	fprintf(fp, "  Max order: %d\n", MAX_K);
+	fprintf(fp, "    EPSILON: %g\n", EPSILON);
+	fprintf(fp, "  Soft zero: %g\n", SOFT_ZERO);
+	fprintf(fp, "-------------------------------------------------------------\n");	// *30
+
 	for (int i = 0; i < (int)polys->size(); i++)
 	{
 		fprintf(fp, "[%+10.6f, %+10.6f]: ", (*divs)[i], (*divs)[i + 1]);
-		for (auto it : *(*polys)[i])
-			fprintf(fp, "%+10.6f, ", fixed_to_float(it.first, fxp_f) * fixed_to_float(it.second, fxp_f));
+		// for (auto it : *(*polys)[i])
+		for (int j = 0; j < (*polys)[i]->size(); j++)
+		{
+			auto it = (*(*polys)[i])[j];
+			double coef = fixed_to_float(it.first, fxp_f) * fixed_to_float(it.second, fxp_f);
+			fprintf(fp, " %c %.6f", coef < 0.0 ? '-' : '+', std::abs(coef));
+			if (j > 1)
+				fprintf(fp, "x^%d", j);
+			else if (j > 0)
+				fprintf(fp, "x");
+			fprintf(fp, "");
+		}
 		fprintf(fp, "\n");
 	}
 
